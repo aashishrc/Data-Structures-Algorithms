8fa34fdcd7c5ec19a69d300bdfbb0c43b2a23aef
Aashish Chaple <aashishrc001@gmail.com>
2024-05-12 15:30:45 -0400

    Time: 78 ms (23.56%) | Memory: 16.9 MB (86.44%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.py
+++ 152-maximum-product-subarray/maximum-product-subarray.py
@@ -3,16 +3,7 @@ class Solution:
         max_prod = -99999
         prefix = 0
         suffix = 0
-        # Bruteforce : works for 187/190 cases
-        # for i in range(len(nums)):
-        #     prod = nums[i]
-        #     for j in range(i+1,len(nums)):
-        #         prod = prod*nums[j]
-        #         max_prod = max(prod, max_prod)
-        #     max_prod = max(prod, nums[i], max_prod)
-        # return max_prod
 
-        # using dp
         for i in range(len(nums)):
             if prefix == 0:
                 prefix = 1
@@ -22,3 +13,14 @@ class Solution:
             suffix *= nums[len(nums) - i - 1]
             max_prod = max(max_prod, max(prefix, suffix))
         return max_prod
+
+                # Bruteforce : works for 187/190 cases
+        # for i in range(len(nums)):
+        #     prod = nums[i]
+        #     for j in range(i+1,len(nums)):
+        #         prod = prod*nums[j]
+        #         max_prod = max(prod, max_prod)
+        #     max_prod = max(prod, nums[i], max_prod)
+        # return max_prod
+
+        # using dp
