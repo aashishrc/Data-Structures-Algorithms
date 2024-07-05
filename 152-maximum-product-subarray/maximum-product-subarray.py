adffaccc232ac02d098d51b2f01335b831cb0131
Aashish Chaple <aashishrc001@gmail.com>
2024-07-05 18:21:36 -0400

    Time: 72 ms (55.57%) | Memory: 17 MB (29.34%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.py
+++ 152-maximum-product-subarray/maximum-product-subarray.py
@@ -1,26 +1,14 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        max_prod = -99999
-        prefix = 0
-        suffix = 0
-
+        pre = 1
+        suf = 1
+        maxi = nums[0]
         for i in range(len(nums)):
-            if prefix == 0:
-                prefix = 1
-            if suffix == 0:
-                suffix =1
-            prefix *= nums[i]
-            suffix *= nums[len(nums) - i - 1]
-            max_prod = max(max_prod, max(prefix, suffix))
-        return max_prod
-
-                # Bruteforce : works for 187/190 cases
-        # for i in range(len(nums)):
-        #     prod = nums[i]
-        #     for j in range(i+1,len(nums)):
-        #         prod = prod*nums[j]
-        #         max_prod = max(prod, max_prod)
-        #     max_prod = max(prod, nums[i], max_prod)
-        # return max_prod
-
-        # using dp
+            if pre == 0:
+                pre = 1
+            if suf == 0:
+                suf = 1
+            pre *= nums[i]
+            suf *= nums[len(nums) - i - 1]
+            maxi = max(maxi, max(pre,suf))
+        return maxi
