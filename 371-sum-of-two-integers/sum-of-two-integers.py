96ff9c76cc43297a6d6c65cfc44156b4aa628a04
Aashish Chaple <aashishrc001@gmail.com>
2024-07-21 22:08:07 -0400

    Time: 42 ms (9.45%) | Memory: 16.5 MB (14.58%) - LeetSync

--- 371-sum-of-two-integers/sum-of-two-integers.py
+++ 371-sum-of-two-integers/sum-of-two-integers.py
@@ -1,17 +1,18 @@
 class Solution:
     def getSum(self, a: int, b: int) -> int:
         MAX = 0xFFFFFFFF
-        # # If the result is negative, get the two's complement positive equivalent
-        # MASK = 0x80000000
+        # If the result is negative, get the two's complement positive equivalent
+        MASK = 0x80000000
 
-        while (b & MAX) > 0:
+        while b != 0:
             # Carry now contains common set bits of a and b
-            carry = (a & b) <<1  
+            carry = a & b
 
             # Sum of bits of a and b where at least one of the bits is not set
-            a = (a ^ b)
+            a = (a ^ b) & MAX
 
             # Carry is shifted by one so that adding it to a gives the required sum
-            b = carry
+            b = (carry << 1) & MAX
+
         # If a is negative, return the negative equivalent in 32 bits representation
-        return (a & MAX) if b > 0 else a
+        return a if a < MASK else ~(a ^ MAX)
