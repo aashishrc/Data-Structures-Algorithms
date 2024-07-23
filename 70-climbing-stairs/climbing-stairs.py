0f713e33c8d878eaa80e467240fdeba7387636af
Aashish Chaple <aashishrc001@gmail.com>
2024-07-23 16:46:11 -0400

    Time: 33 ms (68.04%) | Memory: 16.7 MB (13.17%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.py
@@ -0,0 +1,25 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n <= 2:
+            return n
+        else:
+            tp = n //2
+            print("tp:", tp)
+            counter = 0
+            #add 1 way at the end when there are 0 2's and all 1's
+            for i in range(1, tp + 1):
+                op = n - i*2
+                print("op: ", op)
+                tot_places = op + i
+                print("tot: ", tot_places)
+                #code to find all possibilities
+                counter += self.calc_poss(tot_places, op, i)
+                print("c: ",counter)
+            return counter + 1
+
+    def calc_poss(self, tot_places, op, tp):
+        return self.factorial(tot_places)//(self.factorial(op)*self.factorial(tp)) 
+
+    def factorial(self,n):
+        # single line to find factorial
+        return 1 if (n==1 or n==0) else n * factorial(n - 1) 
