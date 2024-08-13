da257e6b2e9ffaae4e537d4aaef782fc7c02e837
Aashish Chaple <aashishrc001@gmail.com>
2024-08-13 15:04:45 -0400

    Time: 38 ms (56.70%) | Memory: 16.5 MB (11.80%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -1,7 +1,23 @@
 class Solution:
     def fib(self, n: int) -> int:
+        fib = []
+        fib.append(0)
+        fib.append(1)
         if n == 0:
             return 0
         elif n ==1:
-            return 1
-        return self.fib(n-1) + self.fib(n-2)
+            return 1    
+        else:
+            # print(fib)
+            for i in range(2, n +1):
+                fib.append(fib[-2] + fib[-1])
+        return fib[n] 
+        
+        
+        
+        #recursive
+        # if n == 0:
+        #     return 0
+        # elif n ==1:
+        #     return 1
+        # return self.fib(n-1) + self.fib(n-2)
