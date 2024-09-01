441c957dae16f11a5538aa3abadc803ffadd0425
Aashish Chaple <aashishrc001@gmail.com>
2024-09-01 18:18:02 -0400

    Time: 33 ms (71.73%) | Memory: 16.5 MB (66.82%) - LeetSync

--- 468-validate-ip-address/validate-ip-address.py
+++ 468-validate-ip-address/validate-ip-address.py
@@ -1,28 +1,33 @@
-import re
-class Solution(object):
-    def validIPAddress(self, queryIP):
-        """
-        :type queryIP: str
-        :rtype: str
-        """
-        # Regex pattern for validating IPv4
-        ipv4_pattern = re.compile(r'^((2[0-4][0-9]|25[0-5]|1[0-9]{2}|[1-9]?[0-9])\.){3}'
-                                r'(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])$')
-        
-        # Regex pattern for validating IPv6
-        ipv6_pattern = re.compile(r'^([0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}$')
-
-        if ipv4_pattern.match(queryIP):
-            return "IPv4"
-        elif ipv6_pattern.match(queryIP):
-            return "IPv6"
-        else:
+class Solution:
+    def validIPAddress(self, queryIP: str) -> str:
+        if ':' in queryIP:
+            #implementation of ipv6
+            subnets = queryIP.split(':')
+            if len(subnets) == 8:
+                for i in subnets:
+                    if len(i) <= 4 and len(i) >= 1:
+                        print(i)
+                        for j in i.lower():
+                            if 'a' <= j <= 'f' or '0' <= j <= '9':
+                                continue
+                            # if ord(j) >= 48 and ord(j) <= 57 or ord(j) >= 97 and ord(j) <= 102:
+                            #     continue
+                            else:
+                                return "Neither"
+                    else:
+                        return "Neither"
+                return "IPv6"
             return "Neither"
-
-# # Example usage:
-# print(validIPAddress("192.168.1.1"))       # Output: IPv4
-# print(validIPAddress("2001:0db8:85a3:0000:0000:8a2e:0370:7334"))  # Output: IPv6
-# print(validIPAddress("192.168.01.1"))      # Output: Neither
-# print(validIPAddress("02001:0db8:85a3:0000:0000:8a2e:0370:7334")) # Output: Neither
-
-        
+        else:
+            #ipv4
+            subnets = queryIP.split('.')
+            if len(queryIP) > 15:
+                return "Neither"
+            if len(subnets) == 4:
+                for i in subnets:
+                    if not i.isdigit() or not int(i) >= 0 or not int(i) <=255 or str(int(i)) != i:
+                        return "Neither"
+                    #add condition to check for leading zeroes
+                return "IPv4"
+            return "Neither"
+            
