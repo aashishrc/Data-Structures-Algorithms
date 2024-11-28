f43c81c117cd7be15838c07836b59c1724d8a74e
Aashish Chaple <aashishrc001@gmail.com>
2024-11-27 23:58:33 -0500

    Time: 4 ms (10.52%) | Memory: 17.1 MB (5.91%) - LeetSync

--- 91-decode-ways/decode-ways.py
+++ 91-decode-ways/decode-ways.py
@@ -1,20 +1,34 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        if not s or s[0] == '0':
-            return 0
+        dp = {len(s) : 1}
 
-        n = len(s)
-        prev2, prev1 = 1, 1  # Equivalent to dp[i-2] and dp[i-1]
+        for i in range(len(s) - 1, -1, -1):
+            if s[i] == "0":
+                dp[i] = 0
+            else:
+                dp[i] = dp[i+1]
+            
+            if (i+1) < len(s) and (s[i] =="1" or s[i] == "2" and s[i+1] in "0123456"):
+                dp[i] += dp[i+2]
+        return dp[0] 
 
-        for i in range(1, n):
-            current = 0
-            if s[i] != '0':
-                current += prev1
-            if 10 <= int(s[i-1:i+1]) <= 26:
-                current += prev2
-            prev2, prev1 = prev1, current
 
-        return prev1
+        #optimized
+        # if not s or s[0] == '0':
+        #     return 0
+
+        # n = len(s)
+        # prev2, prev1 = 1, 1  # Equivalent to dp[i-2] and dp[i-1]
+
+        # for i in range(1, n):
+        #     current = 0
+        #     if s[i] != '0':
+        #         current += prev1
+        #     if 10 <= int(s[i-1:i+1]) <= 26:
+        #         current += prev2
+        #     prev2, prev1 = prev1, current
+
+        # return prev1
 
 
     # def numDecodings(self, s: str) -> int:
