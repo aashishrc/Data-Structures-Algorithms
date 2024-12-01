15556aa456f87f7cd32a1062d67cd24764a1bb7b
Aashish Chaple <aashishrc001@gmail.com>
2024-12-01 17:55:57 -0500

    Time: 0 ms (100.00%) | Memory: 17.4 MB (5.22%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,20 +1,30 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        if n <= 2:
+        if n<= 2:
             return n
-        else:
-            tp = n //2
-            counter = 0
-            #add 1 way at the end when there are 0 2's and all 1's
-            for i in range(1, tp + 1):
-                op = n - i*2
-                #code to find all possibilities
-                counter += self.calc_poss(op + i, op, i)
-            return counter + 1
+        dp = [-1]*(n+1)
+        dp[1] = 1
+        dp[2] = 2
 
-    def calc_poss(self, tot_places, op, tp):
-        return self.factorial(tot_places)//(self.factorial(op)*self.factorial(tp)) 
+        for i in range(3, n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+        return dp[n]
+        
+    #     if n <= 2:
+    #         return n
+    #     else:
+    #         tp = n //2
+    #         counter = 0
+    #         #add 1 way at the end when there are 0 2's and all 1's
+    #         for i in range(1, tp + 1):
+    #             op = n - i*2
+    #             #code to find all possibilities
+    #             counter += self.calc_poss(op + i, op, i)
+    #         return counter + 1
 
-    def factorial(self,n):
-        # single line to find factorial
-        return 1 if (n==1 or n==0) else n * factorial(n - 1) 
+    # def calc_poss(self, tot_places, op, tp):
+    #     return self.factorial(tot_places)//(self.factorial(op)*self.factorial(tp)) 
+
+    # def factorial(self,n):
+    #     # single line to find factorial
+    #     return 1 if (n==1 or n==0) else n * factorial(n - 1) 
