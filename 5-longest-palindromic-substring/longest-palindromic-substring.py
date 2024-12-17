ca8ec5873c17a3185a2f02b9c84d1af5e48005cc
Aashish Chaple <aashishrc001@gmail.com>
2024-12-17 12:33:49 -0500

    Time: 307 ms (54.72%) | Memory: 17.9 MB (18.47%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -1,19 +1,44 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        ans = s[0]
+        resIdx = 0
+        resLen = 0
+
         for i in range(len(s)):
-            for j in range(len(s) - 1, -1, -1):
-                if s[i] == s[j]:
-                    k = i
-                    l = j
-                    while l >= k and s[l] == s[k]:
-                        k +=1
-                        l -=1
-                    if l > k:
-                        continue
-                    else:
-                        if j - i + 1 > len(ans):
-                            print(i, j)
-                            ans = s[i:j+1]  
-                        break
-        return ans
+            # odd length
+            l, r = i, i
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                if (r - l + 1) > resLen:
+                    resIdx = l
+                    resLen = r - l + 1
+                l -= 1
+                r += 1
+
+            # even length
+            l, r = i, i + 1
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                if (r - l + 1) > resLen:
+                    resIdx = l
+                    resLen = r - l + 1
+                l -= 1
+                r += 1
+
+        return s[resIdx : resIdx + resLen]
+                
+        #bruteforce        
+        # ans = s[0]
+        # for i in range(len(s)):
+        #     for j in range(len(s) - 1, -1, -1):
+        #         if s[i] == s[j]:
+        #             k = i
+        #             l = j
+        #             while l >= k and s[l] == s[k]:
+        #                 k +=1
+        #                 l -=1
+        #             if l > k:
+        #                 continue
+        #             else:
+        #                 if j - i + 1 > len(ans):
+        #                     # print(i, j)
+        #                     ans = s[i:j+1]  
+        #                 break
+        # return ans
