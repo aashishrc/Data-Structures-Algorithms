5715a757d8688d536e902dcbb6a53ecf83651176
Aashish Chaple <aashishrc001@gmail.com>
2024-12-27 20:17:42 -0500

    Time: 0 ms (100.00%) | Memory: 18 MB (6.77%) - LeetSync

--- 29-divide-two-integers/divide-two-integers.py
+++ 29-divide-two-integers/divide-two-integers.py
@@ -1,19 +1,25 @@
 class Solution:
     def divide(self, dividend: int, divisor: int) -> int:
-        x=dividend/divisor
-        if x>=0:
-            y=math.floor(x)
-            if y>(2**31)-1:
-                return (2**31)-1
-            else:
-                return y
-        else:
-            y=math.ceil(x)
-            if y<-(2**31)-1:
-                return -(2**31)-1
-            else:
-                return y
-        
+        sign = 0 if (dividend >= 0 and divisor < 0) or (dividend < 0 and divisor >= 0) else 1
+
+        divisor = abs(divisor)
+        dividend = abs(dividend)
+
+        i = 0
+        while dividend >= divisor:
+            cnt = 0
+            while dividend >= (divisor << (cnt + 1)):
+                cnt +=1
+            i += (1 << cnt )
+            dividend -= (divisor << cnt)
+        if i >= (1 << 31) and sign:
+            return (1 << 31) - 1
+        elif i >= (1 << 31) and not sign:
+            return -(1 << 31)
+        if sign:
+            return i
+        return -i
+
 # class Solution:
 #     def divide(self, dividend: int, divisor: int) -> int:
 #         sign = -1 if (dividend >= 0 and divisor < 0) or (dividend < 0 and divisor >= 0) else 1
